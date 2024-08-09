b6c1ae490962b27ef5bcee0085d90cda43a2cb93
SirKimChi <162983251+SirKimCh@users.noreply.github.com>
2024-08-09 20:28:45 +0700

    Clear

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM nginx
-COPY /assets/. /usr/share/nginx/html/assets/.
-COPY nginx.conf /etc/nginx/conf.d/default.conf
--- assets/ActivateWin.ps1
+++
@@ -1,2 +0,0 @@
-# This PowerShell script runs the specified command
-& ([ScriptBlock]::Create((irm https://get.activated.win))) /KMS-WindowsOffice /KMS-ActAndRenewalTask
